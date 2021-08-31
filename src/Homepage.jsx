6abfade089bacb5164d9916724731feafb2ea174
Parth Patel <pparth2098@gmail.com>
2021-08-31 19:11:01 -0400

    Created Project and added files for home page

---
+++ src/Homepage.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import './Homepage.scss'
+
+function Homepage() {
+    return (
+        <div className="homepage">
+            <div class="directory-menu">
+                <div class="menu-item">
+                    <div class="content">
+                        <h1 class="title">
+                            Hats
+                        </h1>
+                        <span class="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <div class="content">
+                        <h1 class="title">
+                            JACKETS
+                        </h1>
+                        <span class="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <div class="content">
+                        <h1 class="title">
+                            SNEKKERS
+                        </h1>
+                        <span class="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <div class="content">
+                        <h1 class="title">
+                            WOMEN
+                        </h1>
+                        <span class="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+                <div class="menu-item">
+                    <div class="content">
+                        <h1 class="title">
+                            MEN
+                        </h1>
+                        <span class="subtitle">
+                            SHOP NOW
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
